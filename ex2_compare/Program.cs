b31de7eacc071ced48cc07063ce707dc6f2853e7
Q310-BV <q310@bv.net.ru>
2023-07-21 15:53:14 +0400

    Добавлены 2 и 4 задание

---
+++ ex2_compare/Program.cs
@@ -0,0 +1,15 @@
+﻿// Ждем два числа сравниваем и показываем какое результат сравнения
+Console.WriteLine("Жду первое число: ");
+int num1 = int.Parse(Console.ReadLine () ?? "");
+Console.WriteLine("Жду второе число: ");
+int num2 = int.Parse(Console.ReadLine () ?? "");
+
+// Сравнивем числа
+if (num1 == num2) Console.WriteLine($"Число <{num1}> и <{num2}> равны ");
+else if (num1 > num2) Console.WriteLine($"среди чисел <{num1}> и <{num2}> максимальное <{num1}>");
+else Console.WriteLine($"Среди чисел <{num1}> и <{num2}> максимальное <{num2}>");
+
+// тестовый пример
+//      a = 5; b = 7 ->  max = 7
+//      a = 2; b = 10 -> max = 10
+//      a = -9; b = -3 -> max = -3
---
+++ ex4_max/Program.cs
@@ -0,0 +1,21 @@
+﻿// Ждем три числа, сравниваем и показываем результат сравнения
+
+// организуем ввод трех чисел
+Console.WriteLine("Жду первое число: ");
+int num1 = int.Parse(Console.ReadLine () ?? "");
+Console.WriteLine("Жду второе число: ");
+int num2 = int.Parse(Console.ReadLine () ?? "");
+Console.WriteLine("Жду третье число: ");
+int num3 = int.Parse(Console.ReadLine () ?? "");
+
+// Сравнивем числа
+int numMax = num1;
+if (num2 > numMax) numMax = num2 ;
+if (num3 > numMax) numMax = num3 ;
+
+Console.WriteLine($"Среди чисел <{num1}>, <{num2}> и <{num3}> максимальное <{numMax}>");
+
+// тестовый пример
+//      2, 3, 7 -> 7
+//      44, 5, 78 -> 78
+//      22, 3, 9 -> 22
