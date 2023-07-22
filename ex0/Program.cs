34d2701bc2330d15505f6ea41296b11085bd56e8
Q310-BV <q310@bv.net.ru>
2023-07-22 12:41:09 +0400

    Удаление лишнего

--- ex0/Program.cs
+++ ex0/Program.cs
@@ -1,6 +1,4 @@
-﻿using System.Net;
-
-
+﻿
 Console.WriteLine("Ожидание целого числа:");
 //string  myInput = Console.ReadLine() ?? "";
 //int number = int.Parse(myInput);
@@ -10,7 +8,3 @@ int result = number * number ;
 
 //Console.WriteLine ("Result = "+result);
 Console.WriteLine ($"Result : {number} * {number} = {result}");
-
-
-//string Host = System.Net.Dns.GetHostName();
-//string IP = System.Net.Dns.Use GetHostEntry(Host).AddressList[0].ToString();
