71bddef0f299caf2c079bc3a72f88f99c4d481fa
Q310-BV <q310@bv.net.ru>
2023-07-22 12:29:01 +0400

    Добавлено задание 8

---
+++ ex8_all_even/Program.cs
@@ -0,0 +1,16 @@
+﻿// Ждем число
+Console.WriteLine("Жду число: ");
+int numTest = int.Parse(Console.ReadLine () ?? "");
+// Переберем все числа от 1 до числа numTest
+int cur = 2;
+Console.Write($"Четные числа от <1> до <{numTest}> :");  
+while (cur < numTest)
+{
+    Console.Write($" {cur}");
+    cur =cur +2;  
+}    
+Console.WriteLine("");
+
+// Примеры для проверки
+//      5 -> 2, 4
+//      8 -> 2, 4, 6, 8
