85b3705dc7bfc9b357aa99ad1b17c75516c86d8f
Q310-BV <q310@bv.net.ru>
2023-07-22 11:59:49 +0400

    Завершено задание 6

--- ex6_even/Program.cs
+++ ex6_even/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Ждем число
+Console.WriteLine("Жду число: ");
+int numTest = int.Parse(Console.ReadLine () ?? "");
+// Проверим на четность (т.е деление на два без остатка)
+if (numTest % 2 == 0 ) Console.WriteLine($"Число: {numTest} - Четное"); 
+else Console.WriteLine($"Число: {numTest} - Нечетное");  
+    
+
+// Примеры для проверки
+//      4 -> да
+//      -3 -> нет
+//      7 -> нет
